9290d7f4ce32f71b76719268c6bf34d7c6e3ca94
Student <owen.kiteck@stu.barren.kyschools.us>
2022-02-17 12:55:16 -0600

    created index.html and script.js. connected script.js to html

---
+++ script.js
@@ -0,0 +1,5 @@
+// TEXT ADVENTURE 
+
+alert(`Welcome to Luigi's Mansion 3 1/2! Press OK to start!`)
+
+alert(`Luigi gets a random invitation to a very fancy ball and decides he deserves a break from the plumbing. He arrives at the mansion, but something is off... `)
